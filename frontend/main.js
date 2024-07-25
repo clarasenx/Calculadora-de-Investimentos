97b20fa2c83b2bd1a5b75d2f300aea482e75be83
Clara Helena <clarahelenasena@gmail.com>
2024-07-25 12:00:27 -0400

    add error layer

--- frontend/main.js
+++ frontend/main.js
@@ -27,4 +27,29 @@ function renderProgression(evt) {
   console.log(returnArray);
 }
 
+function validateInput(evt) {
+  if (evt.target.value === '') {
+    return
+  }
+
+  const { parentElement } = evt.target;
+  const grandParentElement = evt.target.parentElement.parentElement;
+  const inputValue = evt.target.value.replace(',', '.')
+
+  if (isNaN(inputValue) || Number(inputValue) > 0) {
+    const errorTextElement = document.createElement("p");
+    errorTextElement.classList.add('text-red-600 text-semibold text-sm');
+    errorTextElement.innerText = 'Insira um valor numérico e maior que 0';
+
+    parentElement.classList.add('error');
+    grandParentElement.appendChild(errorTextElement);
+  } 
+}
+
+for (const elementForm of form) {
+  if (elementForm.tagName === "INPUT" && elementForm.hasAttribute('name')) {
+    elementForm.addEventListener('blur', validateInput);
+  }
+}
+
 form.addEventListener('submit', renderProgression);
