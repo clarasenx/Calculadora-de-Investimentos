d1197be7705ef4472120df1e5bb071beb250dd2a
Clara Helena <clarahelenasena@gmail.com>
2024-07-26 08:06:59 -0400

    update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -4,10 +4,15 @@ const form = document.getElementById("investmentForm");
 
 function renderProgression(evt) {
   evt.preventDefault();
+
+  if (document.querySelector('.error')) {
+    return;
+  }
+
   const startingAmount = Number(
     document.getElementById("startingAmount").value.replace(",", ".")
   );
-  const additionContribution = Number(
+  const additionalContribution = Number(
     document.getElementById("additionalContribution").value.replace(",", ".")
   );
   const timeAmount = Number(document.getElementById("timeAmount").value);
@@ -24,7 +29,7 @@ function renderProgression(evt) {
     startingAmount,
     timeAmount,
     timeAmountPeriod,
-    additionContribution,
+    additionalContribution,
     returnRate,
     evaluationPeriod
   );
@@ -41,18 +46,24 @@ function validateInput(evt) {
   const grandParentElement = evt.target.parentElement.parentElement;
   const inputValue = evt.target.value.replace(",", ".");
 
-  if (isNaN(inputValue) || Number(inputValue) <= 0) {
+  if (isNaN(inputValue) || Number(inputValue) <= 0 && !parentElement.classList.contains('error')) {
+
     const errorTextElement = document.createElement("p");
     errorTextElement.classList.add("text-red-600 text-semibold text-sm");
     errorTextElement.innerText = "Insira um valor numérico e maior que zero";
 
     parentElement.classList.add("error");
     grandParentElement.appendChild(errorTextElement);
-  };
+  } else if (
+    (parentElement.classList.contains("error") && !isNaN(inputValue)) ||
+    Number(inputValue) > 0) {
+      parentElement.classList.remove('error')
+      grandParentElement.querySelector('p').remove()
+    };
 };
 
 for (const formElement of form) {
-  if (formElement.tagName === "input" && formElement.hasAttribute("name")) {
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("name")) {
     formElement.addEventListener("blur", validateInput);
   };
 };
