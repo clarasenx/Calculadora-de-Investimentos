699dd2e33535a362ccd4bf19885cb3d1016874f1
Clara Helena <clarahelenasena@gmail.com>
2024-07-26 09:38:37 -0400

    fix tailwind export

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.{html, js}", "./src/**/*.{html, js}"],
+  content: ["./**/*.{html,js}", "./*.{html,js}"],
   theme: {
     extend: {},
   },
