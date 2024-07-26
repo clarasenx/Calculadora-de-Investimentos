6a8a51c5e661a6afb07089fc2d3e633b40b5b511
Clara Helena <clarahelenasena@gmail.com>
2024-07-26 09:16:08 -0400

    fix form error message bug

--- frontend/main.js
+++ frontend/main.js
@@ -46,20 +46,14 @@ function validateInput(evt) {
   const grandParentElement = evt.target.parentElement.parentElement;
   const inputValue = evt.target.value.replace(",", ".");
 
-  if (
-    isNaN(inputValue) ||
-    (Number(inputValue) <= 0 && !parentElement.classList.contains("error"))
-  ) {
+  if ((isNaN(inputValue) && (Number(inputValue) <= 0) || !parentElement.classList.contains("error"))) {
     const errorTextElement = document.createElement("p");
-    errorTextElement.classList.add("text-red-600 text-semibold text-sm");
+    errorTextElement.classList.add("text-red-600");
     errorTextElement.innerText = "Insira um valor numérico e maior que zero";
 
     parentElement.classList.add("error");
     grandParentElement.appendChild(errorTextElement);
-  } else if (
-    (parentElement.classList.contains("error") && !isNaN(inputValue)) ||
-    Number(inputValue) > 0
-  ) {
+  } else if ((parentElement.classList.contains("error") && !isNaN(inputValue)) && Number(inputValue) > 0) {
     parentElement.classList.remove("error");
     grandParentElement.querySelector("p").remove();
   }
